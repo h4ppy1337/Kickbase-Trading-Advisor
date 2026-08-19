3c3f11489fc07885fe1dbe412973bc47fbce58ed
h4ppy1337 <45348437+h4ppy1337@users.noreply.github.com>
2026-08-19 11:14:58 +0200

    Update others.py

--- kickbase_api/others.py
+++ kickbase_api/others.py
@@ -51,9 +51,8 @@ def get_achievement_reward(token, league_id, achievement_id):
 
     url = f"{BASE_URL}/leagues/{league_id}/user/achievements/{achievement_id}"
     data = get_json_with_token(url, token)
-    print(f"Achievement response for {achievement_id}: {data}")
 
-    amount = data["ac"]
-    reward = data["er"]
-
+    amount = data.get("ac", 0)
+    reward = data.get("er", 0)
+    
     return amount, reward
